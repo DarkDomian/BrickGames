d46b6966c2db64294cf6aeed4e1a66285f303876
Demian "trelawnm" Domozhirov <darkdomian@gmail.com>
2025-12-15 14:56:43 +0300

    feat(cli): drawing game's HUD

--- gui/cli/src/cli.c
+++ gui/cli/src/cli.c
@@ -1,18 +1,61 @@
 #include "cli.h"
 
 int main(void) {
-  // initialize ncurses
-  initscr();    // Start curses mode
-  raw();        // suspend, interrupt and quit char goes direct to the program
-  noecho();     // block printing the user input
-  keypad(stdscr, TRUE); // enable hande the F1..F12, arrows, etc.
+  BrickUI ui = {
+    .game_field = NULL,
+    .info_bar = NULL
+  };
 
-  WINDOW *new_window;
+  if (BrickGameInit(50, &ui) == ERROR) {
+    fprintf(stderr, "Faild to initialize ncurses mode...");
+    return ERROR;
+  }
+  
+  DrawUI(&ui);
+  
+  // GameLoop
+  char ch;
+  while ((ch = wgetch(ui.game_field)) != 'q') {
+    continue;
+  }
 
-  printw("Hello World !!!");	/* Print Hello World		  */
-  refresh();			/* Print it on to the real screen */
-  getch();			/* Wait for user input */
-  endwin();			/* End curses mode		  */
+  BrickGameCleanup(&ui);
+}
 
-  return 0;
+
+int BrickGameInit(const int delay, BrickUI *ui) {
+  if (initscr() == NULL) return ERROR;
+  
+  noecho();
+  curs_set(0);
+  keypad(stdscr, TRUE);
+  timeout(delay);
+
+  ui->game_field = newwin(PLAY_FIELD_HEIGHT + 2, PLAY_FIELD_WIDTH * 2 + 2, 0, 0);
+  ui->info_bar = newwin(PLAY_FIELD_HEIGHT + 2, 12, 0, PLAY_FIELD_WIDTH * 2 + 2);
+  
+  return SUCCESS;
+}
+
+void BrickGameCleanup(BrickUI *ui) {
+  delwin(ui->game_field);
+  delwin(ui->info_bar);
+  endwin();
+}
+
+void DrawUI(BrickUI *ui) {
+  box(ui->game_field, 0, 0);
+  box(ui->info_bar, 0, 0);
+
+  mvwprintw(ui->info_bar, 1, 2, "%8s", "score");
+  // score = derwin(side_bar, 1, 8, 2, 2);
+  mvwprintw(ui->info_bar, 4, 2, "%8s", "hi-score");
+  // hi_score = derwin(side_bar, 1, 8, 5, 2);
+  mvwprintw(ui->info_bar, 7, 2, "%8s", "next");
+  // next = derwin(side_bar, 4, 8, 8, 2);
+  
+  mvwprintw(ui->info_bar, 19, 2, "%8s", "pause");
+
+  wrefresh(ui->game_field);
+  wrefresh(ui->info_bar);
 }
--- gui/cli/src/cli.h
+++ gui/cli/src/cli.h
@@ -2,5 +2,48 @@
 #define CLI_H
 
 #include <ncurses.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-#endif // CLI_H
+// #include "tetris_lib.h"
+
+#define SUCCESS 0
+#define ERROR 1
+
+#define PLAY_FIELD_HEIGHT 20
+#define PLAY_FIELD_WIDTH 10
+
+/** Structure with pointers to gaming windows, exists for easy data share between functions */
+typedef struct {
+    WINDOW *game_field;
+    WINDOW *info_bar;
+} BrickUI;
+
+/**
+ * @brief initialize curses mode and setup the `BrickUI` by allocate memory for windows
+ *
+ * @param delay passed to `timeout()` ncurses function
+ * @param ui pointer to BrickUI for windows managment
+ * @note If delay is negative, curses uses a blocking read, waiting indefinitely
+ * for input. If delay is zero, the read is non-blocking; an input character
+ * reading function returns ERR if no input is pending. If delay is positive, an
+ * input character reading function blocks for delay milliseconds, and returns
+ * ERR if the delay elapses and there is still no input pending.
+ */
+int BrickGameInit(const int delay, BrickUI *ui);
+
+/**
+ * @brief Call at the end of work. Deallocate windwos memory, fihish curses mode.
+ * 
+ * @param ui pointer to BrickUI for windows managment 
+ */
+void BrickGameCleanup(BrickUI *ui);
+
+/**
+ * @brief Called once for drawing game-HUD, boxes around game field and titels for information
+ * 
+ * @param ui pointer to BrickUI for windows managment
+ */
+void DrawUI(BrickUI *ui);
+
+#endif  // CLI_H
